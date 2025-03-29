df4be3c934c5b3b92eaaa631e47f4b35324502a7
LuckGets <kasdies12@gmail.com>
2025-03-29 23:07:50 +0700

    Making submission service

--- src/resources/submissions/submissions.service.ts
+++ src/resources/submissions/submissions.service.ts
@@ -1,6 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Service } from 'src/common/base-class';
+import { Submission } from './domain/submission.domain';
+import { CreateSubmissionInput } from './dto/create.dto';
 
 @Injectable()
-export class SubmissionsService {
-  public executeCode() {}
+export class SubmissionsService extends Service<Submission> {
+  private mockSubmission: Submission = new Submission({
+    id: '',
+    code: '',
+    createdAt: '',
+    questionId: '',
+    updatedAt: '',
+    userId: '',
+  });
+
+  private executeCode() {}
+
+  override create(data: CreateSubmissionInput): Promise<Submission> {
+    return Promise.resolve(this.mockSubmission);
+  }
+  getById(id: string): Promise<Submission> {
+    return Promise.resolve(this.mockSubmission);
+  }
+  getMany(): Promise<Submission[]> {
+    return Promise.resolve([this.mockSubmission]);
+  }
+  update(
+    data: Partial<Omit<Submission, 'id'>>,
+    id: string,
+  ): Promise<Submission> {
+    return Promise.resolve(this.mockSubmission);
+  }
 }
